386bdef6f8601c779ad0d52c9bb7b58499ced061
Abdul Basit Minhas <47935105+Technical-Abm@users.noreply.github.com>
2022-10-26 05:54:45 -0700

    Create course_1.py

---
+++ hello,world/course_1.py
@@ -0,0 +1,7 @@
+# course 1 basic python | hello world
+print("Hello, World!")
+
+# you can print hello world using single quatation mark | doubel quotation mark | tripple quatation mark
+print('Hello, World!')
+print("Hello, World!")
+print("""Hello, World!""")
