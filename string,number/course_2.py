826b331e4ee57003a02b9d274df1aebfa3e55c37
Abdul Basit Minhas <47935105+Technical-Abm@users.noreply.github.com>
2022-10-26 06:06:50 -0700

    Create course_2.py

---
+++ string,number/course_2.py
@@ -0,0 +1,21 @@
+# string and number
+
+name = "abm"
+age = 22
+age_formula = str(age)
+print(name,age_formula)
+
+user = 22
+age_str = str(user)
+one_name = "technical abm "+age_str
+print(one_name)
+
+# message for user | using string method | tripple quatation marks
+
+msg = """ Hello world, python is a general purpose and high level programming language """
+print(msg)
+
+nm =  "technical abm"
+age = 22
+age_click = str(age)
+print(" Your name is "+nm+" Your real age is "+age_click)
