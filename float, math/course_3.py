942d901aef5ca7e22a86cfb2b5dfd0bada4d8ef2
Abdul Basit Minhas <47935105+Technical-Abm@users.noreply.github.com>
2022-10-26 06:23:55 -0700

    Create course_3.py

---
+++ float, math/course_3.py
@@ -0,0 +1,36 @@
+# float and math
+
+nbr = 3
+print(float(str(nbr)))
+
+# output | 3.0
+
+number = 3
+number_click = str(number)
+print(float(number_click))
+
+# output | 3.0
+
+nm_1 = 5
+nm_2 = 7
+nm_3 = 10
+print(float(str(nm_1)))
+print(float(str(nm_2)))
+print(float(nm_3))
+
+# output | 5.0 | 7.0 | 10.0
+
+math = 2 + 4
+print(math)
+
+# output | 6
+
+math_2 = 2 + 5 + 7 + 8 * 22 / 7
+print(math_2)
+
+# output | 39.142
+
+wind = 5 + 5 * 5 / 5
+print(wind)
+
+# output | 10.0
