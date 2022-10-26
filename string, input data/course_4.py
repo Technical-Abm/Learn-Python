a2816bb134878eacb96ce3ea81641cb59836f66c
Abdul Basit Minhas <47935105+Technical-Abm@users.noreply.github.com>
2022-10-26 06:34:15 -0700

    Update course_4.py

--- string, input data/course_4.py
+++ string, input data/course_4.py
@@ -1,1 +1,18 @@
+# string and input user data
+name = input("what is your name : ")
+print(name)
 
+name = input(" Input your name : ")
+print("Hello "+name)
+
+name = input(" enter your name : ")
+msg = " welcome to python programming "
+print(name,msg)
+
+name = input("name : ")
+age = str(input("age : "))
+print(" your name is {}".format(name))
+print(" Your real age is {}".format(age))
+
+msg = input(" enter your any msg : ")
+print(f" Your msg here | {msg}")
